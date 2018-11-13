34903a3f20313a86803827887bf95b74a680beb4
kuikuikuizzZ <root@gpu-03.prd.caicloud.io>
2018-11-13 16:09:01 +0800

    add multi gpu

--- densenet/densenet_lib.py
+++ densenet/densenet_lib.py
@@ -2,17 +2,21 @@
 
 import os 
 import sys
-sys.path.insert(0, os.getcwd())
+sys.path.append(os.path.dirname(os.getcwd()))
 
-from . import  keys
-from . import densenet
+import  densenet.keys as keys
+import densenet.densenet as densenet
 import difflib
 import numpy as np
 from PIL import Image,ImageOps
+import cv2
 from keras.layers import Input
 from keras.models import Model
 from imp import reload
-
+from keras.backend import ctc_decode,eval
+import tensorflow as tf
+import keras.backend.tensorflow_backend as KTF
+import time
 characters = keys.alphabet_union[:]
 characters = characters[1:] + u'卍'
 
@@ -31,6 +35,11 @@ class Densenet_keras(object):
         self.height = height
         self.nClass = len(characters)
         self.characters = characters
+        if KTF._get_available_gpus():
+            self.config = tf.ConfigProto()
+            self.config.gpu_options.per_process_gpu_memory_fraction = 0.9
+            self.session = tf.Session(config=self.config)
+            KTF.set_session(self.session)
         self.basemodel = self.get_model(height=self.height,nClass=self.nClass)
         self.basemodel.load_weights(model_path)
         
@@ -41,22 +50,36 @@ class Densenet_keras(object):
         return basemodel
         
     def recognize(self,img):
+        img_L = img.astype(np.float32)
+        img_L = img_L[:,:,0]*114/1000 + img_L[:,:,1]* 587/1000 + img_L[:,:,2]* 299/1000 
+        scale = img_L.shape[0]*1.0/32.0
+        w = int(img_L.shape[1]/scale)
+        img = cv2.resize(img_L,(w,32),cv2.INTER_AREA)
+        img = np.array(img)/ 255.0 - 0.5
+        X = img.reshape((32, w, 1))
+        X = np.array([X])
+        KTF.set_session(self.session)
+        y_pred = self.basemodel.predict(X)
+        y_pred = y_pred[:, :, :]
+        out = self.decode(y_pred)  ##
+        return out
+    
+    
+    def predict(self,img):        
         im = Image.fromarray(img)
         im = im.convert('L')
         scale = im.size[1] * 1.0 / 32
         w = int(im.size[0] / scale)
         im = im.resize((w, 32),Image.ANTIALIAS)
         img = np.array(im).astype(np.float32) / 255.0 - 0.5
-#         print(img.shape,scale)
         X = img.reshape((32, w, 1))
         X = np.array([X])
-        print(X.shape)
+        start = time.time()
+        KTF.set_session(self.session)
         y_pred = self.basemodel.predict(X)
-        y_pred = y_pred[:, :, :]
-        out = self.decode(y_pred)  ##
-        return out
+        return y_pred
     
-    def decode(self,pred):
+    def decode(self,pred,with_confidence=False):
         text = pred.argmax(axis=2)[0]
         length = len(text)
         char_list = []
@@ -65,13 +88,71 @@ class Densenet_keras(object):
             # 这里text[i] != n就是处理出来的分隔符，text[i - 1] == text[i]处理重复的字符
             if text[i] != n and (not (i > 0 and text[i - 1] == text[i])):
                     char_list.append(self.characters[text[i]])
-        return u''.join(char_list)
+                 
+        if with_confidence:
+            pred_max = pred.max(axis=2)[0]
+            confidence_list = [] 
+            for i in range(length):
+                # 这里text[i] != n就是处理出来的分隔符，text[i - 1] == text[i]处理重复的字符
+                if text[i] != n and (not (i > 0 and text[i - 1] == text[i])):    
+                        confidence_list.append(pred_max[i])
+            length_confidence = len(confidence_list)
+            
+            if length_confidence < 3:
+                confidence = np.mean(confidence_list)
+            else :
+                sorted_confidence = np.sort(confidence_list)
+                confidence = np.mean(sorted_confidence[:length_confidence//2])
+            return u''.join(char_list),confidence
+        return u''.join(char_list) 
     
+    def recognize_with_confidence(self,img):
+        im = Image.fromarray(img)
+        im = im.convert('L')
+        scale = im.size[1] * 1.0 / 32
+        w = int(im.size[0] / scale)
+        im = im.resize((w, 32),Image.ANTIALIAS)
+        img = np.array(im).astype(np.float32) / 255.0 - 0.5
+        X = img.reshape((32, w, 1))
+        X = np.array([X])
+        start = time.time()
+        KTF.set_session(self.session)
+        y_pred = self.basemodel.predict(X)
+        y_pred = y_pred[:, :, :]
+        out = self.decode(y_pred,with_confidence=True)  ##
+        return out
+        
     def load_weights(self,model_path):
         self.basemodel.load_weights(model_path)
         print('loaded model')
-    
-    
+        
+    def recognize_on_batch(self,img_list,with_confidence=False):
+        max_w = int(max([32.0/img.shape[0]*img.shape[1] for img in img_list]))
+#         print(max_w)
+#         assert len(img_list) < 128
+        img_batch = np.ones([len(img_list),32,max_w,1])*0.5
+        for i,img in enumerate(img_list):
+            img_L = img.astype(np.float32)
+            if len(img_L.shape) ==3:
+                img_L = img_L[:,:,0]*114/1000 + img_L[:,:,1]* 587/1000 + img_L[:,:,2]* 299/1000 
+            scale = img_L.shape[0]*1.0/32.0
+            w = int(img_L.shape[1]/scale)
+            img = cv2.resize(img_L,(w,32),cv2.INTER_AREA)
+            img = img/ 255.0 - 0.5
+            img = img.reshape((32, w, 1))
+            img_batch[i,:,:w,:] = img
+        KTF.set_session(self.session)
+        y_pred = self.basemodel.predict(img_batch)
+        y_pred = y_pred[:, :, :]
+        out_list = []
+        for i in range(y_pred.shape[0]):
+            y_encode = np.array([y_pred[i,:,:]]) 
+            out = self.decode(y_encode,with_confidence)  ##
+            out_list.append(out)
+        return out_list
+            
+
+
 
 if __name__ == '__main__':
     import cv2
--- train/densenet.py
+++ train/densenet.py
@@ -9,37 +9,37 @@ from keras.regularizers import l2
 from keras.layers.wrappers import TimeDistributed
 
 
-def conv_block(input, growth_rate, dropout_rate=None, weight_decay=1e-4):
-    x = BatchNormalization(axis=-1, epsilon=1.1e-5)(input)
-    x = Activation('relu')(x)
-    x = Conv2D(growth_rate, (3,3), kernel_initializer='he_normal', padding='same')(x)
+def conv_block(input, growth_rate, dropout_rate=None, weight_decay=1e-4,name=None):
+    x = BatchNormalization(axis=-1, epsilon=1.1e-5,name=name+'_0_bn')(input)
+    x = Activation('relu',name=name+'_0_relu')(x)
+    x = Conv2D(growth_rate, (3,3), kernel_initializer='he_normal', padding='same',name=name+'_1_conv')(x)
     if(dropout_rate):
-        x = Dropout(dropout_rate)(x)
+        x = Dropout(dropout_rate,name=name+'_1_dropout')(x)
     return x
 
-def dense_block(x, nb_layers, nb_filter, growth_rate, droput_rate=0.2, weight_decay=1e-4):
+def dense_block(x, nb_layers, nb_filter, growth_rate, droput_rate=0.2, weight_decay=1e-4,name=None):
     for i in range(nb_layers):
-        cb = conv_block(x, growth_rate, droput_rate, weight_decay)
-        x = concatenate([x, cb], axis=-1)
+        cb = conv_block(x, growth_rate, droput_rate, weight_decay,name=name + '_block' + str(i + 1))
+        x = concatenate([x, cb], axis=-1,name=name + '_concat'+ str(i + 1))
         nb_filter += growth_rate
     return x, nb_filter
 
-def transition_block(input, nb_filter, dropout_rate=None, pooltype=1, weight_decay=1e-4):
-    x = BatchNormalization(axis=-1, epsilon=1.1e-5)(input)
-    x = Activation('relu')(x)
+def transition_block(input, nb_filter,dropout_rate=None, pooltype=1, weight_decay=1e-4,name=None):
+    x = BatchNormalization(axis=-1, epsilon=1.1e-5,name=name + '_bn')(input)
+    x = Activation('relu',name=name + '_relu')(x)
     x = Conv2D(nb_filter, (1, 1), kernel_initializer='he_normal', padding='same', use_bias=False,
-               kernel_regularizer=l2(weight_decay))(x)
+               kernel_regularizer=l2(weight_decay),name=name + '_conv')(x)
 
     if(dropout_rate):
-        x = Dropout(dropout_rate)(x)
+        x = Dropout(dropout_rate,name=name + '_dropout')(x)
 
     if(pooltype == 2):
-        x = AveragePooling2D((2, 2), strides=(2, 2))(x)
+        x = AveragePooling2D((2, 2), strides=(2, 2),name=name + '_0_avgpool')(x)
     elif(pooltype == 1):
-        x = ZeroPadding2D(padding = (0, 1))(x)
-        x = AveragePooling2D((2, 2), strides=(2, 1))(x)
+        x = ZeroPadding2D(padding = (0, 1),name=name + '_0_zeropool')(x)
+        x = AveragePooling2D((2, 2), strides=(2, 1),name=name + '_1_avgpool')(x)
     elif(pooltype == 3):
-        x = AveragePooling2D((2, 2), strides=(2, 1))(x)
+        x = AveragePooling2D((2, 2), strides=(2, 1),name=name + '_2_avgpool')(x)
     return x, nb_filter
 
 def dense_cnn(input, nclass):
@@ -50,23 +50,23 @@ def dense_cnn(input, nclass):
     _nb_filter = 64
     # conv 64 5*5 s=2
     x = Conv2D(_nb_filter, (5, 5), strides=(2, 2), kernel_initializer='he_normal', padding='same',
-               use_bias=False, kernel_regularizer=l2(_weight_decay))(input)
+               use_bias=False, kernel_regularizer=l2(_weight_decay),name='conv1/conv')(input)
    
     # 64 + 8 * 8 = 128
-    x, _nb_filter = dense_block(x, 8, _nb_filter, 8, None, _weight_decay)
+    x, _nb_filter = dense_block(x, 8, _nb_filter, 8, None, _weight_decay,name='conv2',)
     # 128
-    x, _nb_filter = transition_block(x, 128, _dropout_rate, 2, _weight_decay)
+    x, _nb_filter = transition_block(x, 128, _dropout_rate, 2, _weight_decay,name='pool2')
 
     # 128 + 8 * 8 = 192
-    x, _nb_filter = dense_block(x, 8, _nb_filter, 8, None, _weight_decay)
+    x, _nb_filter = dense_block(x, 8, _nb_filter, 8, None, _weight_decay,name='conv3')
     # 192 -> 128
-    x, _nb_filter = transition_block(x, 128, _dropout_rate, 2, _weight_decay)
+    x, _nb_filter = transition_block(x, 128, _dropout_rate, 2, _weight_decay,name='pool3')
 
     # 128 + 8 * 8 = 192
-    x, _nb_filter = dense_block(x, 8, _nb_filter, 8, None, _weight_decay)
+    x, _nb_filter = dense_block(x, 8, _nb_filter, 8, None, _weight_decay,name='conv4')
 
-    x = BatchNormalization(axis=-1, epsilon=1.1e-5)(x)
-    x = Activation('relu')(x)
+    x = BatchNormalization(axis=-1, epsilon=1.1e-5,name='bn')(x)
+    x = Activation('relu',name='relu')(x)
 
     x = Permute((2, 1, 3), name='permute')(x)
     x = TimeDistributed(Flatten(), name='flatten')(x)
@@ -76,6 +76,43 @@ def dense_cnn(input, nclass):
     # basemodel.summary()
 
     return y_pred
+
+
+
+def dense_cnn_no_dense(input):
+
+    _dropout_rate = 0.2 
+    _weight_decay = 1e-4
+
+    _nb_filter = 64
+    # conv 64 5*5 s=2
+    x = Conv2D(_nb_filter, (5, 5), strides=(2, 2), kernel_initializer='he_normal', padding='same',
+               use_bias=False, kernel_regularizer=l2(_weight_decay),name='conv1/conv')(input)
+   
+    # 64 + 8 * 8 = 128
+    x, _nb_filter = dense_block(x, 8, _nb_filter, 8, None, _weight_decay,name='conv2',)
+    # 128
+    x, _nb_filter = transition_block(x, 128, _dropout_rate, 2, _weight_decay,name='pool2')
+
+    # 128 + 8 * 8 = 192
+    x, _nb_filter = dense_block(x, 8, _nb_filter, 8, None, _weight_decay,name='conv3')
+    # 192 -> 128
+    x, _nb_filter = transition_block(x, 128, _dropout_rate, 2, _weight_decay,name='pool3')
+
+    # 128 + 8 * 8 = 192
+    x, _nb_filter = dense_block(x, 8, _nb_filter, 8, None, _weight_decay,name='conv4')
+
+    x = BatchNormalization(axis=-1, epsilon=1.1e-5,name='bn')(x)
+    x = Activation('relu',name='relu')(x)
+
+    x = Permute((2, 1, 3), name='permute')(x)
+    flatten = TimeDistributed(Flatten(), name='flatten')(x)
+#     y_pred = Dense(nclass, name='y_pred_out', activation='softmax')(x)
+
+    # basemodel = Model(inputs=input, outputs=y_pred)
+    # basemodel.summary()
+
+    return flatten
 
 def dense_blstm(input):
 
